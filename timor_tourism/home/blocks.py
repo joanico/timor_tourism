5d167b14d50274a55482a17724baf09bbb283733
joanico <joanicobarros@gmail.com>
2019-07-21 11:28:54 +0900

    Fix home page template and add comment ina block

--- timor_tourism/home/blocks.py
+++ timor_tourism/home/blocks.py
@@ -1,8 +1,9 @@
 from wagtail.core import blocks
 from wagtail.images.blocks import ImageChooserBlock
 
-
+# Class ba field nebe fo naran body iha model konaba paragraph nian.
 class TourismRichBlock(blocks.StructBlock): 
+    """Text no align ba paragraph nebe atu add"""
     text = blocks.RichTextBlock(help_text='WYSIWYG text')
     align = blocks.ChoiceBlock(choices=(
                ('left', 'Left'), ('right', 'Right'),
