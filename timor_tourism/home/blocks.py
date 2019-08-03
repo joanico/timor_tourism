5d84c95dc14cb5968ed52d019e76039d31b874a8
joanico <joanicobarros@gmail.com>
2019-08-03 22:56:13 +0900

    create menu

--- timor_tourism/home/blocks.py
+++ timor_tourism/home/blocks.py
@@ -13,3 +13,52 @@ class TourismRichBlock(blocks.StructBlock):
 
     class Meta:
         template = 'home/blocks/tourism_rich_block.html'
+
+
+class BaseLinkBlock(blocks.StructBlock):
+    """
+    Base StructBlock class used to prevent DRY code.
+    """
+    display_text = blocks.CharBlock()
+
+
+class ExternalLinkBlock(BaseLinkBlock):
+    """
+    Block that holds a link to any URL.
+    """
+    link = blocks.URLBlock()
+
+    class Meta:
+        template = 'home/menu/external_link_block.html'
+
+class PageLinkBlock(BaseLinkBlock):
+    """
+    Block that holds a page.
+    """
+    page = blocks.PageChooserBlock()
+
+    class Meta:
+        template = 'home/menu/page_link_block.html'
+
+class LinkChildrenBlock(blocks.StructBlock):
+    """
+    Base childblock for second level children.
+    """
+    children = blocks.StreamBlock(
+            [
+                ('external_link', ExternalLinkBlock()),
+                ('page_link', PageLinkBlock()),
+            ]
+        )
+
+class ExternalLinkWithChildrenBlock(LinkChildrenBlock, ExternalLinkBlock):
+    """
+    Uses LinkChildrenBlock as a mixin to create an ExternalLinkBlock that supports Children.
+    """
+    pass
+
+class PageLinkWithChildrenBlock(LinkChildrenBlock, PageLinkBlock):
+    """
+    Uses LinkChildrenBlock as a mixin to create a PageLinkBlock that supports Children.
+    """
+    pass
